8ed94350c0cb67376438cdf20446ecea580fd685
Hassan Aletreby <hassaneletreby21@gmail.com>
2024-05-03 22:23:22 +0300

    Add darkmode to my project

--- js/js.js
+++ js/js.js
@@ -3,3 +3,10 @@ var nav = document.querySelector(".nav__bottom");
 btnMenu.onclick = function () {
   nav.classList.toggle("disapper");
 }
+// ________________________________________
+var toggle = document.querySelector("#toggleBtn");
+var body = document.querySelector(".html");
+
+toggle.addEventListener("change", function() {
+  body.classList.toggle("dark");
+});
